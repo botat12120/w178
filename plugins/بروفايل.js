76c992ebc7b08b58792f987d02c8c130641698fa
witcherbot1 <witcherbot11@gmail.com>
2024-08-12 20:30:56 +0300

    بروفايل.js

--- plugins/بروفايل.js
+++ plugins/بروفايل.js
@@ -1,35 +1,38 @@
-import { createHash } from 'crypto' 
+import { createHash } from 'crypto'
 import PhoneNumber from 'awesome-phonenumber'
-import fetch from 'node-fetch'
-let handler = async (m, { conn, usedPrefix }) => {
-let pp = 'https://telegra.ph/file/e8b3219035e65f946fd20.jpg'
-//const pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => './src/avatar_contact.png')
-let user = global.db.data.users[m.sender]
-let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-try {
-pp = await conn.getProfilePicture(who)         //pp = await conn.getProfilePicture(who)
-} catch (e) {
+import { canLevelUp, xpRange } from '../lib/levelling.js'
 
-} finally {
-let { name, limit, lastclaim, registered, regTime, age } = global.db.data.users[who]
-//let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let mentionedJid = [who]
-let username = conn.getName(who)
-let prem = global.prems.includes(who.split`@`[0])
-let sn = createHash('md5').update(who).digest('hex')
-let str =
-`*┌───⊷﹝البــروفــايــل﹞+⊷*
-*▢〉🔖‣  الأســـــم :@${who.replace(/@.+/, '')}*
-*▢〉🔗 الرابط:* wa.me/${who.split`@`[0]}
-*▢〉📱‣ رقم الهاتف:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
-${registered ? '*▢〉🎈‣ العـــمـــر:' + age + 'ســـنـــوات*' : ''}
-*▢〉📇‣ مـــســـجـــل : ${registered ? '✅' : '❌'}*
-*└──────────────────⊷*`.trim()
-    conn.sendFile(m.chat, pp, 'pp.jpg', str, fkontak, false, { contextInfo: { mentionedJid }}) 
-  }
+let handler = async (m, { conn, usedPrefix, command}) => {
+
+    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  
+    let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png')
+    let user = global.db.data.users[who]
+    let { name, exp, diamond, lastclaim, registered, regTime, age, level, role, warn } = global.db.data.users[who]
+    let { min, xp, max } = xpRange(user.level, global.multiplier)
+    let username = conn.getName(who)
+    let math = max - xp
+    let prem = global.prems.includes(who.split`@`[0])
+    let sn = createHash('md5').update(who).digest('hex')
+
+    let str = `
+┌───「 PROFILE 」
+▢ 🔖 الاسم: 
+   • ${username} ${registered ? '\n   • ' + name + ' ': ''}
+   • @${who.replace(/@.+/, '')}
+▢ 📱الرقم: ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
+▢ 🔗الرابط: http://wa.me/${who.split`@`[0]}${registered ? '\n▢ 🎈العمر: ' + age + ' سنة' : ''}
+
+▢ 🆙 المستوى : ${level}
+▢ ⬆ XP : المجموع ${exp}
+▢ النقاط المتبقية للارتقاء: ${user.exp - min} / ${xp - user.exp}
+▢ 🏆الدور: ${role}
+▢ 📇 مسجل : ${registered ? 'نعم': 'لا'}
+└──────────────`
+    conn.sendFile(m.chat, pp, 'perfil.jpg', str, m, false, { mentions: [who] })
 }
-handler.help = ['profile [@user]']
-handler.tags = ['xp']
-handler.command = /^بروفايل|profile?$/i
+handler.help = ['perfil']
+handler.tags = ['group']
+handler.command = ['profile', 'perfil','عني','بروفايل','البروفايل'] 
+
 export default handler
