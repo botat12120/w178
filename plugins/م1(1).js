87a295dc5620fbe9ec2073bd20fd104b0b0bc80b
witcherbot1 <witcherbot11@gmail.com>
2024-08-12 23:10:22 +0300

    Update م1(1).js

--- plugins/م1(1).js
+++ plugins/م1(1).js
@@ -68,7 +68,7 @@ let str = `⎔⋅• ━ ╼╃ ⌬〔﷽〕⌬ ╄╾ ━ •⋅⎔
         }
 
         function _0x42ca() {
-            const _0xe1e20e = ['2409147cMTtvV', '70SxKSwY', 'trim', '2708027tOnEUS', '831853GQAjyu', '6ZrUjVF', 'Zenon ⚡ 𝐂𝐎𝐃𝐈𝐍𝐆', '406070MLYaeB', 'https://chat.whatsapp.com/BrQVKh6Kt0X8LuMVWzW9XR', '4YbGMkZ', '840496IPJeXP', '322835aPaRGc', 'VIDEO', '3639410cLSfPv', 'sender', '9LtUyCF'];
+            const _0xe1e20e = ['2409147cMTtvV', '70SxKSwY', 'trim', '2708027tOnEUS', '831853GQAjyu', '6ZrUjVF', '『🔰‹𝚆𝙸𝚃𝙲𝙷𝙴𝚁›🔰』', '406070MLYaeB', 'https://chat.whatsapp.com/Ck3mQdmm0Ty3FMrJSNsG90', '4YbGMkZ', '840496IPJeXP', '322835aPaRGc', 'VIDEO', '3639410cLSfPv', 'sender', '9LtUyCF'];
             _0x42ca = function () {
                 return _0xe1e20e;
             };
