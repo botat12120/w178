7c674a746934c8c191a078c2504431942a3d8015
witcherbot1 <witcherbot11@gmail.com>
2024-08-13 00:52:48 +0300

    Update م3(1).js

--- plugins/م3(1).js
+++ plugins/م3(1).js
@@ -24,12 +24,10 @@ let rtotalreg = Object.values(global.db.data.users).filter(user => user.register
 let more = String.fromCharCode(8206)
 let readMore = more.repeat(850)   
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let str = `⎔⋅• ━ ╼╃ ⌬〔﷽〕⌬ ╄╾ ━ •⋅⎔
+let str = `◉━━━─ •༺ ﷽ ༻• ─━━━◉
 > 𝙍𝙊𝘽 ↳🔰↲ 𝐁𝐎𝐓
 > 〔قسم الـمـطـور┊ ˼‏ ⚡˹ 〕↶
-*⌬∙ • ──╾⊱﹝🔰﹞⊰╼── • ∙⌬*
-            *روب بــ🦍ــوت*
-*⌬∙ • ──╾⊱﹝🔰﹞⊰╼── • ∙⌬*
+*◉━━━─ •༺ 🔰 ༻• ─━━━◉*
 > ⚡ .بريم
 > ⚡ .الغاء-البريم
 > ⚡ .بان
@@ -42,10 +40,9 @@ let str = `⎔⋅• ━ ╼╃ ⌬〔﷽〕⌬ ╄╾ ━ •⋅⎔
 > ⚡ .ادخل
 > ⚡ .ضيف_اكس_بي
 > ⚡ .ضيف_جواهر
-*⌬∙ • ──╾⊱﹝🔰﹞⊰╼── • ∙⌬*
 *~⌬ تــ✍︎ــوقــيــع ↡~*🖋️
 *⌞🔰┊𝙍𝙊𝘽  ス W.C.H┊🔰⌝*
-*⌬∙ • ──╾⊱﹝🔰﹞⊰╼── • ∙⌬*
+*◉━━━─ •༺ 🔰 ༻• ─━━━◉*
 `.trim()
     const _0x3c7cab = _0x5a1d;
 
