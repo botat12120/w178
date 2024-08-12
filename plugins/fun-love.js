1d52752f023d2828b3b12326d198468d05ded69d
witcherbot1 <witcherbot11@gmail.com>
2024-08-12 09:10:34 +0300

    fun-love.js

--- plugins/fun-love.js
+++ plugins/fun-love.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn, command, text }) => {
-let love = `*✨❤️ نسبه الحب ❤️✨*
-*نسبة الحب ${text} ليك ✨❤️* *${Math.floor(Math.random() * 100)}%* *من 100%*
-*اطلب منها ان تكون حبيبة لك وتعملون شي ههههههههه✨؟*
+let love = `*✨🫡 نسبه الاخوة 🫡 ✨*
+*نسبة الاخوة ${text} ليك ✨🫡* *${Math.floor(Math.random() * 100)}%* *من 100%*
+*كفو تنطح كفو !!*
 `.trim()
 m.reply(love, null, { mentions: conn.parseMention(love) })}
 handler.help = ['love']
 handler.tags = ['fun']
-handler.command = /^(الحب)$/i
+handler.command = /^(الاخوة)$/i
 export default handler
