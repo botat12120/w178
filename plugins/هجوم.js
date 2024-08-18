0e6abe000139daf8ede715074652f59dc729a4f0
witcherbot1 <witcherbot11@gmail.com>
2024-08-18 06:03:09 +0300

    هجوم.js

--- plugins/هجوم.js
+++ plugins/هجوم.js
@@ -1,18 +1,18 @@
 let ro = 3000
 let handler = async (m, { conn, usedPrefix, command}) => {
 let time = global.db.data.users[m.sender].lastrob + 7200000
-if (new Date - global.db.data.users[m.sender].lastrob < 7200000) throw `*⏱️ مهلا انتظر ${msToTime(time - new Date())} عشان تسرق تاني*`
+if (new Date - global.db.data.users[m.sender].lastrob < 7200000) throw *⏱️ مهلا انتظر ${msToTime(time - new Date())} عشان تسرق تاني*
 let who
 if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
 else who = m.chat
-if (!who) throw `*[❗] منشن للي هتسرقه يحرامي*`
-if (!(who in global.db.data.users)) throw `*[❗] المستخدم غير موجود في قاعدة البيانات الخاصة بي.*`
+if (!who) throw *[❗] منشن للي هتسرقه يحرامي*
+if (!(who in global.db.data.users)) throw *[❗] المستخدم غير موجود في قاعدة البيانات الخاصة بي.*
 let users = global.db.data.users[who]
 let rob = Math.floor(Math.random() * ro)
-if (users.exp < rob) return m.reply(`😔 @${who.split`@`[0]} لديه أقل من *${ro} xp*\nلا تسرق رجل فقير":`, null, { mentions: [who] })    
+if (users.exp < rob) return m.reply(😔 @${who.split@[0]} لديه أقل من *${ro} xp*\nلا تسرق رجل فقير":, null, { mentions: [who] })    
 global.db.data.users[m.sender].exp += rob
 global.db.data.users[who].exp -= rob 
-m.reply(`*‣ انت سرقت ${rob} XP من @${who.split`@`[0]}*`, null, { mentions: [who] })
+m.reply(*‣ انت سرقت ${rob} XP من @${who.split@[0]}*, null, { mentions: [who] })
 global.db.data.users[m.sender].lastrob = new Date * 1
 }
 handler.help = ['rob']
